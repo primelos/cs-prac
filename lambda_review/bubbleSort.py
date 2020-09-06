fbcce2b1916d7de2635f2effaab61674d8f7c9ae
Ale <alearanibar@gmail.com>
2020-09-06 00:13:03 -0700

    bubble working

--- lambda_review/bubbleSort.py
+++ lambda_review/bubbleSort.py
@@ -1,4 +1,13 @@
 def bubble(z):
     for num in range(len(z) -1, 0, -1):
         for i in range(num):
-            
+            if z[i] > z[i + 1]:
+                # 10, 9
+                temp = z[i]
+                z[i] = z[i + 1]
+                z[i + 1] = temp
+    return z
+test1 = [5,88,3,14,64,2,3,9]
+print(test1)
+print(bubble(test1))
+print(test1)
