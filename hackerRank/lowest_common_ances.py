62dd1861a5cb7c37c34b3d97878506e98e01ffa1
Ale <alearanibar@gmail.com>
2020-11-03 12:30:45 -0800

    lca code

---
+++ hackerRank/lowest_common_ances.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+# GIVEN CODE ABOVE --^-^-^-^-^
+
+def lca(root, v1, v2):
+    if root == None:
+        return None
+    
+    if root.info < v1 and root.info < v2:
+        return lca(root.right, v1, v2)
+    if root.info > v1 and root.info > v2:
+        return lca(root.left, v1, v2)
+    return root
